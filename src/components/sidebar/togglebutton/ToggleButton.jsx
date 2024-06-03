b2417dc35daec360b9a7856616d3b668e210f684
GULSHAN Kumar <67850876+gulshan8383@users.noreply.github.com>
2024-06-03 22:29:29 +0530

    Update ToggleButton.jsx

--- src/components/sidebar/togglebutton/ToggleButton.jsx
+++ src/components/sidebar/togglebutton/ToggleButton.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const ToggleButton = ({ setOpen }) => {
   return (
-    <button onClick={() => setOpen((prev) => !prev)}>
+    <button onClick={() => setOpen((prev) => !prev)} className='flex justify-center items-center'>
       <svg width="23" height="23" viewBox="0 0 23 23">
         <motion.path strokeWidth="3"
           stroke="black"
