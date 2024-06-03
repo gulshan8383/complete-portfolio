9ec6627e73630d2eed442ca6449ae20efd246f1a
GULSHAN Kumar <67850876+gulshan8383@users.noreply.github.com>
2024-06-03 22:33:39 +0530

    Update Carasoul.jsx

--- src/components/carasoul/Carasoul.jsx
+++ src/components/carasoul/Carasoul.jsx
@@ -30,8 +30,8 @@ const Carasoul = () => {
                 <img src={d.img} alt="" className='image  rounded-full' />
               </div>
               <div className='descdiv'>
-                <p className='name'>{d.Name}</p>
-                <p className='descrip'>{d.desc}</p>
+                <p className='name text-md'>{d.Name}</p>
+                <p className='descrip text-xs'>{d.desc}</p>
               </div>
             </div>
           ))}
@@ -45,7 +45,7 @@ const Carasoul = () => {
 
 const newdata = [
   {
-    Name: `Gulshan kumar`,
+    Name: `SELF PORTFOLIO`,
     img: `/web1.jpg`,
     desc: `I'm UI/UX Developer and I'M freelancer and i'm using React js, Scss, Javascript, Jquey, MySql, if you can create website then contact me. in my colledge days i'm participate my activity and win. In hackthon i'm participate 3 times and win the trophy 2 times.`
   },
@@ -60,7 +60,7 @@ const newdata = [
     desc: `My computer skills is I'm UI/UX Designer, Frontend developer, javascript developer, rectjs developer, i'm good in teamwork, i'm very dedicated to my work, i'm very friendy behaviour man. In present time i'm learn database, backend lenguages in future goes to mernstack developer   `
   },
   {
-    Name: `client Sumit`,
+    Name: `SUMIT MEHENDI ART`,
     img: `/web2.jpg`,
     desc: `My client sumit and he was mehendi designer. and i'm create website for my client all infomation and design of mehendi are available. my client is happy to see my work. anyone need you business to online market then contact me. i will give you good discounts.  `
   },
